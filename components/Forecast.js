4a69e687c932a57c397e5872efcb1a26c80de3e9
Gabriella Tavares <gabriellatatavares@gmail.com>
2021-02-11 18:27:19 +0000

    forecast done

---
+++ components/Forecast.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import {StyleSheet, View, Image, Text} from 'react-native'
+
+export default class Forecast extends React.Component {
+  state = { }
+
+  componentDidMount(){
+
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+    let date = new Date(this.props.detail.dt * 1000)
+    let dayName = days[date.getDay()]
+    let day = date.getDate()
+
+    this.setState({
+      dayName,
+      day,
+    })
+  }
+  render() {
+    return(
+        <View style = {styles.card}>
+            <View style = {styles.container}>
+                <View style = {styles.innerContainer}>
+                    <Text style = {styles.date}>{this.state.dayName}</Text>
+                </View>
+
+                <Image style = {styles.image} source = {{uri:"https://openweathermap.org/img/wn/" + this.props.detail.weather[0].icon + ".png"}}/>
+                
+                <View style = {styles.innerContainer}>
+                    <Text style = {styles.numberDay}>{Math.round(this.props.detail.temp.day)}</Text>
+                    <Text style = {styles.numberNight}> {Math.round(this.props.detail.temp.night)}</Text>
+                </View>
+            </View>  
+        </View>
+    )
+}
+}
+
+const styles = StyleSheet.create({
+card: {
+    backgroundColor: 'transparent',
+    borderColor: '#FAF0E6',
+    borderWidth: 1,
+},
+container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    paddingBottom: 0,
+},
+date: {
+    fontSize: 15,
+    fontWeight: 'bold',
+    paddingLeft: 20,
+    color: 'rgb(220, 116, 84)'
+},
+innerContainer: {
+    flexDirection: 'row',
+    textAlign: 'left',
+    width: '30%',
+},
+numberDay: {
+    fontSize: 20,
+    paddingLeft: 15,
+    color: 'rgb(220, 116, 84)'
+},
+numberNight: {
+    fontSize: 20,
+    paddingLeft: 35,
+    color: 'rgb(220, 116, 84)'
+
+},
+image: {
+    width: 50,
+    height: 50,
+},
+});
---
+++ components/WeatherInfo.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { 
+  View, 
+  Text, 
+  StyleSheet, 
+  Image } from 'react-native'
+import { Picker } from '@react-native-community/picker'
+
+
+export default function WeatherInfo({currentWeather, unitsSystem, setUnitsSystem}) {
+  const {
+    main: {temp, feels_like, temp_min, temp_max},
+    weather: [details],
+    name
+  } = currentWeather
+  const {icon, main } = details
+
+  const iconUrl = `https://openweathermap.org/img/wn/${icon}@4x.png`
+  return (
+    <>
+    <View style={styles.WeatherInfo}>
+      <Text style={styles.textPrimary}>{name}</Text>
+      <Text style={styles.textSecondary}>{main}</Text>
+      <Image 
+      style={styles.weatherIcon}
+      source={{uri: iconUrl}}/>
+        {/* <Text>Min: {temp_min}°. Max: {temp_max}°</Text> */}
+        <Text>Feels like: {feels_like}°</Text>
+      <View style={{
+        flexDirection: 'row',
+        alignItems: 'center',
+      }}>
+        <Text style={styles.textPrimary}>{temp}</Text>
+        <Picker style={{flex: 0.3,} } selectedValue={unitsSystem} onValueChange={(item) => setUnitsSystem(item)} >
+          <Picker.Item label="°C" value='metric'/>
+          <Picker.Item label="°F" value='imperial'/>
+        </Picker>
+      </View>
+    </View>
+  </>
+  )
+}
+
+const styles = StyleSheet.create({
+  WeatherInfo: {
+    alignItems: 'center',
+  },
+ 
+  weatherIcon: {
+    width: 100,
+    height: 100,
+  },
+  textPrimary: {
+    fontSize: 40,
+    fontWeight: '500',
+  },
+  textSecondary: {
+    fontSize: 20,
+    marginTop: 10
+  },
+
+})
